4197991f7a667ca4c9774b3d4a028a303c5b4c0b
Rodrigo Giudice <giudicerodrigo@hotmail.com>
2021-09-30 19:11:50 -0300

    espacios entre slides editados

--- js/iniciarSwiperConLoopyAutoplay.js
+++ js/iniciarSwiperConLoopyAutoplay.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
+    spaceBetween: 0,
     centeredSlides: true,
     autoplay: {
       delay: 2500,
--- js/iniciarSwiperSinLoop.js
+++ js/iniciarSwiperSinLoop.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 0,
   loop: false,
   pagination: {
     el: ".swiper-pagination",
